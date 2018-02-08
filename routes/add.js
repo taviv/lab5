887d57d99fd63a19b0f4ec0ce34cb23ff973e82c
Vivian Ta <vivianta@vivians-mbp-2.dynamic.ucsd.edu>
2018-02-08 03:41:01 -0800

    update

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,17 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	var newname = req.query.name;
+    var newdescription = req.query.description;
+    var newimage = 'http://lorempixel.com/400/400/people';
+    var newfriend = {}; 
+    newfriend["name"] = newname;
+	newfriend["description"] = newdescription;
+    newfriend["imageURL"] = newimage;
+//    console.log(newname)
+//    console.log(newdescrip)
+//    console.log(newimage)
+    data.friends.push(newfriend);
+    res.render('index',data);
+    console.log(newfriend)
  }
